91b7a3fcf8257af3e4b277f5c5a23e2b29a2b484
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-10-19 13:40:06 -0300

    validação de usuário e retorno github

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,18 +3,29 @@ import axios from "axios";
 /*
     Receber code(String)
     Recuperar o acesso_token no github
+    Recuperar infos do uer no github
     Verificar se o usuario existe no DB
         Sim = Gera um tokem
         Não = Cria no DB, gera um token
     Retorna o token com infos do user
 */
 
+interface IAccessTokenResponse{
+    access_token: string
+}
+
+interface IUserResponse{
+    avatar_url: string,
+    login: string,
+    id: number,
+    name: string
+}
 
 class AuthenticateUserService {
     async execute(code: string) {
         const url = "https://github.com/login/oauth/access_token";
 
-        const reponse = await axios.post(url, null, {
+        const { data:acessoTokenResponse} = await axios.post<IAccessTokenResponse>(url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENT_ID,
                 client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -25,7 +36,13 @@ class AuthenticateUserService {
             }
         });
 
-        return reponse.data;
+        const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+            headers: {
+                authorization: `Bearer ${acessoTokenResponse.access_token}`
+            }
+        });
+
+        return response.data;
     }
 }
 
